84bcb231518dcdc99aed184a433dffe817d58d66
svm15032007-hub <svm15032007@gmail.com>
2026-02-03 11:19:14 +0530

    Create exp1activity.py

---
+++ exp1/exp1activity.py
@@ -0,0 +1,21 @@
+"""
+Created on Tue Feb  3 10:41:34 2026
+
+@author: User
+"""
+
+num1 = int(input("Enter First Number:"))
+num2 =int(input("Enter Second Number:"))
+
+sum=num1+num2
+print("Addition=",sum)
+
+
+sub=num1-num2
+print("Substraction=",sub) 
+
+mu1=num1*num2
+print("Multiplication=",mu1)
+
+div=num1/num2
+print("Division=",div)
